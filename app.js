d41eb7bf63c039bf28aa868dbfa8002eb5153d9b
Rawbethy <91237481+Rawbethy@users.noreply.github.com>
2022-11-02 14:52:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dboperation = require('./dboperation');
+const bodyParser = require('body-parser');
+const router = express.Router();
 const app = express();
 
 const port = 3000;
@@ -7,5 +9,8 @@ const port = 3000;
 dboperation.getUsers().then(res=> {
     console.log(res);
 })
+
+app.use(bodyParser.json());
+
 
 app.listen(port, () => console.log("Listening on port " + port));
