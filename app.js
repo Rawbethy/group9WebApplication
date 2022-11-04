78ae2db01d829348c15556763f42c07ceecc37de
Rawbethy <91237481+Rawbethy@users.noreply.github.com>
2022-11-03 20:28:46 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const dboperation = require('./dboperation');
 const bodyParser = require('body-parser');
-const router = express.Router();
 const app = express();
 
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+
+app.post('/insertForm', (req, res) => {
+    console.log("INSERT INTO [dbo].[products] VALUES("+req.body.productID+", \""+req.body.productName+"\", \""+req.body.productType+"\", \""+req.body.productDesc+"\", "+req.body.price+", "+req.body.productQuantity+", "+req.body.discount+");");
+});
 
 dboperation.getUsers().then(res => {
     console.log(res);
-})
+});
 
-app.use(bodyParser.json());
-
+const port = 5500;
 
 app.listen(port, () => console.log("Listening on port " + port));
--- dboperation.js
+++ dboperation.js
@@ -5,7 +5,6 @@ const getUsers = async() => {
     try {
         let pool = await sql.connect(config);
         let users = pool.request().query("SELECT * from [dbo].[user];");
-        console.log(users);
         return users
     }
     catch(error) {
