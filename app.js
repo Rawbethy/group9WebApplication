6c1c0919dd6ce6d36aecb0018d432cfcfa124d6f
Rawbethy <91237481+Rawbethy@users.noreply.github.com>
2022-11-04 00:31:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const dboperation = require('./dboperation');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const app = express();
-const sql = require('mssql');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 
 app.post('/insertForm', (req, res) => {
-    console.log("INSERT INTO [dbo].[products] (productID,fullname,productType,prodDesc,price,productQuantity,discount) VALUES("+req.body.productID+", \'"+req.body.productName+"\', \'"+req.body.productType+"\', \'"+req.body.productDesc+"\', "+req.body.price+", "+req.body.productQuantity+", "+req.body.discount+");");
-    var query = "INSERT INTO [dbo].[products] (productID,fullname,productType,prodDesc,price,productQuantity,discount) VALUES("+req.body.productID+", \'"+req.body.productName+"\', \'"+req.body.productType+"\', \'"+req.body.productDesc+"\', "+req.body.price+", "+req.body.productQuantity+", "+req.body.discount+");";
+    console.log("INSERT INTO [dbo].[products] VALUES(\'"+req.body.productName+"\', \'"+req.body.productType+"\', \'"+req.body.productDesc+"\', "+req.body.price+", "+req.body.productQuantity+", "+req.body.discount+");");
+    var query = "INSERT INTO [dbo].[products] VALUES(\'"+req.body.productName+"\', \'"+req.body.productType+"\', \'"+req.body.productDesc+"\', "+req.body.price+", "+req.body.productQuantity+", "+req.body.discount+");";
     dboperation.insertQuery(query);
 });
 
